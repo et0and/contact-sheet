fee654ad5f42aece273db881db81064d429801cc
Tom Hackshaw <42124348+et0and@users.noreply.github.com>
2024-02-07 01:19:39 +1300

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ accepted_extensions = (".jpg", ".jpeg", ".png")
 def main():
     st.title('Contact Sheet Generator')
 
-    output_file = 'contact_sheet.jpg'
+    output_dir = 'contact_sheets'
     image_files = st.file_uploader('Upload images', type=accepted_extensions, accept_multiple_files=True)
 
-    if st.button('Generate Contact Sheet'):
+    if st.button('Generate Contact Sheets'):
         if image_files:
             # Create the directory if it doesn't exist
             os.makedirs("uploaded_images", exist_ok=True)
@@ -24,10 +24,12 @@ def main():
                     f.write(image_file.getbuffer())
                 image_paths.append(file_path)
 
-            create_contact_sheet(image_paths, output_file)
+            num_sheets = create_contact_sheet(image_paths, output_dir)
 
-            # Display the contact sheet
-            st.image(output_file, caption='Generated Contact Sheet', use_column_width=True)
+            # Display the contact sheets
+            for i in range(1, num_sheets + 1):
+                sheet_path = os.path.join(output_dir, f'contact_sheet_{i}.jpg')
+                st.image(sheet_path, caption=f'Generated Contact Sheet {i}', use_column_width=True)
 
             # Clean up uploaded images
             for file_path in image_paths:
